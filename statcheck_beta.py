02786b81cf27d18a34a2d02af703cddf2641110f
hoocdarealest <70364696+Hoocs151@users.noreply.github.com>
2023-02-20 22:12:52 +0700

    + Improve embed

--- statcheck_beta.py
+++ statcheck_beta.py
@@ -5,6 +5,9 @@ import time
 import configparser
 import os
 import requests
+import colorama
+from colorama import Fore, Style
+import datetime
 
 CONFIG_DEFAULTS = {
     'webhook_url': 'YOUR_WEBHOOK_URL_HERE',
@@ -26,23 +29,23 @@ update_interval = int(config['cac']['update_interval'])
 note = config['cac']['note']
 update_check_enabled = config['cac']['update_check_enabled']
 
-print(""" 
+# Initialize colorama to work on Windows OS
+colorama.init()
+
+title = """ 
  ▄  █ ████▄ ████▄ ▄█▄      ▄▄▄▄▄   
 █   █ █   █ █   █ █▀ ▀▄   █     ▀▄ 
 ██▀▀█ █   █ █   █ █   ▀ ▄  ▀▀▀▀▄   
 █   █ ▀████ ▀████ █▄  ▄▀ ▀▄▄▄▄▀    
    █              ▀███▀            
-  ▀                                
-""")
-frame_char = "="
+  ▀                                """
 frame_width = 50
-top_line = frame_char * frame_width
-print(f"\n{top_line}")
-print("\nConfig Settings:")
-print(f"Webhook URL: {webhook_url}")
-print(f"Update Interval: {update_interval} seconds")
-print(f"Note: {note}")
-print(f"Roblox Update Detected: {update_check_enabled}\n")
+
+print(Fore.LIGHTWHITE_EX + f"{title.center(frame_width)}" + Style.RESET_ALL)
+print(Fore.LIGHTMAGENTA_EX + f"Webhook URL: {webhook_url}" + Style.RESET_ALL)
+print(Fore.LIGHTMAGENTA_EX + f"Update Interval: {update_interval} seconds" + Style.RESET_ALL)
+print(Fore.LIGHTMAGENTA_EX + f"Note: {note}" + Style.RESET_ALL)
+print(Fore.LIGHTMAGENTA_EX + f"Roblox Update Detected: {update_check_enabled}\n" + Style.RESET_ALL)
 
 roblox_version = None
 
@@ -56,12 +59,12 @@ while True:
             
             if roblox_version != latest_version:
                 roblox_version = latest_version
-                print(f'[ + ] New Roblox update detected --> Closing Roblox account manager!')
+                print(f'{Fore.GREEN}[ + ] New Roblox update detected --> Closing Roblox account manager!{Style.RESET_ALL}')
                 for process in psutil.process_iter(['name', 'cmdline']):
                     if 'RobloxAccountManager.exe' in process.info['name'] and 'NoUpdatePrompt' in process.info['cmdline']:
                         if psutil.pid_exists(process.pid):
                             process.terminate()
-                print(f'[ + ] Current Roblox version: {latest_version}')
+                print(f'{Fore.GREEN}[ + ] Current Roblox version: {latest_version}{Style.RESET_ALL}')
 
         except Exception as e:
             pass
@@ -73,10 +76,26 @@ while True:
 
     webhook = discord_webhook.DiscordWebhook(url=webhook_url)
 
-    embed = discord_webhook.DiscordEmbed(title="VPS STAT CHECK", description=note, color=0x7289da)
+
+    embed = discord_webhook.DiscordEmbed(
+        title="VPS STAT CHECK",
+        description=note,
+        color=0x7289da
+    )
     embed.set_image(url="attachment://screenshot.png")
-    embed.set_footer(text=f"Dữ liệu sẽ được tải lên sau mỗi {update_interval} giây")
-    embed.add_embed_field(name="<:hoocstok:1045320042230841356> Roblox Tabs", value=f"`{num_roblox_tabs}`")
+    embed.set_footer(
+        text=f"Dữ liệu sẽ được tải lên sau mỗi {update_interval} giây - {datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}"
+    )
+    embed.add_embed_field(
+        name="<:hoocstok:1045320042230841356> Roblox Tabs",
+        value=f"`{num_roblox_tabs}`",
+        inline=True
+    )
+    embed.add_embed_field(
+        name="⏱ Time",
+        value=f"`{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}`",
+        inline=True
+    )
 
     webhook.add_file(file=open(screenshot_path, "rb"), filename="screenshot.png")
 
